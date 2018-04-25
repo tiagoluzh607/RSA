74e9f41e113268725893abbce89287647a2a43cd
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-04-25 11:29:10 -0300

    Algoritmo de Euclides Estendido - Concluído no Console

---
+++ public_html/js/euclides_extendido.js
@@ -0,0 +1,50 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+console.log("testes de log");
+var d = EuclidesExtendido(352,7);
+console.log(d);
+
+function EuclidesExtendido( qq, e){
+    
+    var s = 0; var s_anterior = 1;
+    var t = 1; var t_anterior = 0;
+    var r = e; var r_anterior = qq;
+    
+    while(r != 1) {
+                 
+        //Calcula quociente
+        var quociente = Math.trunc(r_anterior / r);        
+        
+        //Calcula resto
+        var auxr = r_anterior;
+        r_anterior = r;
+        r = auxr % r;
+        
+        //Calcula S
+        var auxs = s_anterior;
+        s_anterior = s;
+        s = auxs - quociente * s;
+
+        //Calcula T
+        var auxt = t_anterior;
+        t_anterior = t;
+        t = auxt - quociente * t;
+
+        //Exibe no Console a Cada interação
+        console.log("quocinete: "+quociente+" resto: "+r+" valor S: "+s+" valor T: "+t);
+    }
+    
+    //retorno somente quem atende a condição (d*e) mod qq = 1
+    if( (t*e)%qq == 1 ){
+        return t;
+    }else if ( (s*e)%qq == 1 ) {
+        return s;
+    }
+
+    
+}
